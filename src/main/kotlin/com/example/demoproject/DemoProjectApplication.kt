8d4639359adb9d7e48fa1231ee57a50e584b48fc
begsaha <sudip.saha@beg.de>
2023-04-28 08:32:32 +0200

    first commit

---
+++ src/main/kotlin/com/example/demoproject/DemoProjectApplication.kt
@@ -0,0 +1,19 @@
+package com.example.demoproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+@RestController
+class DemoProjectApplication
+
+@GetMapping("/version")
+fun getVersion():String{
+	return "version"
+}
+
+fun main(args: Array<String>) {
+	runApplication<DemoProjectApplication>(*args)
+}
